59bd7b5ab34564fac97e0560f28211e4d7c78fe1
Matiaslagost7 <matiaslagost7@gmail.com>
2025-11-21 22:34:47 -0300

    Fix app labels and AUTH_USER_MODEL for Render deployment

--- concesionaria/myapp_conces/apps.py
+++ concesionaria/myapp_conces/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class MyappConcesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'concesionaria.myapp_conces'
+    label = 'myapp_conces'
--- concesionaria/myapp_login/apps.py
+++ concesionaria/myapp_login/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class MyappLoginConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'concesionaria.myapp_login'
+    label = 'myapp_login'
